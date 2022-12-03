76df1c3bb5fea3aa50508407f01e6a52ef70bda2
Dmytro Virych <dmytro.virych@space-whale.io>
2022-12-03 22:32:45 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Calculator app',
       home: CalculatorPage(
-        builder: (calculator) {
+        builder: (Calculator calculator) {
           return CalculatorPageBody(
             calculator: calculator,
           );
